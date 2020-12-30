200ea5b37b97fc49fc1f4b42308f68161287d174
Mohammed ElHomosy <mohammed.elhomosy@aptiv.com>
2020-12-30 22:39:56 +0200

    pep8/ delete route

--- backend/src/api.py
+++ backend/src/api.py
@@ -18,34 +18,42 @@ CORS(app)
 '''
 # db_drop_and_create_all()
 
-## ROUTES
+# ROUTES
 '''
 @TODO implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
+    returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks or appropriate status code
+        indicating reason for failure
 '''
+
+
 @app.route('/drinks')
 @requires_auth('get:drinks')
 def drinks(jwt):
     print(jwt)
     return 'Not Implemented'
 
+
 '''
 @TODO implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
+    returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks or appropriate status code
+        indicating reason for failure
 '''
+
+
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
 def drinks-detail(jwt):
     print(jwt)
     return 'Not Implemented'
+
 
 '''
 @TODO implement endpoint
@@ -53,14 +61,18 @@ def drinks-detail(jwt):
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+    returns status code 200 and json {"success": True, "drinks": drink}
+        where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
-@app.route('/drinks')
+
+
+@app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def post_drinks(jwt):
     print(jwt)
     return 'Not Implemented'
+
 
 '''
 @TODO implement endpoint
@@ -70,14 +82,18 @@ def post_drinks(jwt):
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+    returns status code 200 and json {"success": True, "drinks": drink}
+        where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
-@app.route('/drinks')
+
+
+@app.route('/drinks', methods=['PATCH'])
 @requires_auth('patch:drinks')
 def patch_drinks(jwt):
     print(jwt)
     return 'Not Implemented'
+
 
 '''
 @TODO implement endpoint
@@ -86,67 +102,95 @@ def patch_drinks(jwt):
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
+    returns status code 200 and json {"success": True, "delete": id}
+        where id is the id of the deleted record or appropriate
+        status code indicating reason for failure
 '''
-@app.route('/drinks')
-@requires_auth('delete:drinks')
-def delete_drinks(jwt):
-    print(jwt)
-    return 'Not Implemented'
 
-## Error Handling
+
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_specific_drink(id):
+    try:
+        drink = Drink.query.filter(Drink.id == id).one_or_none()
+        # drink = Drink(title=req_title, recipe=req_recipe)
+        if drink is None:
+            abort(404)
+        drink.delete()
+        return jsonify({
+          'success': True,
+          'deleted': drink.id,
+        }), 200
+    except Exception:
+        abort(422)
+
+
+# Error Handling
 '''
 error handling for unprocessable entity
 '''
+
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 422,
                     "message": "unprocessable"
                     }), 422
 
+
 '''
 error handling for resource not found
 '''
+
+
 @app.errorhandler(404)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 404,
                     "message": "resource not found"
                     }), 404
 
+
 '''
 error handling for Forbidden client error
 '''
+
+
 @app.errorhandler(403)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 403,
                     "message": "forbidden"
                     }), 403
 
+
 '''
 error handling for Unauthorized client
 '''
+
+
 @app.errorhandler(401)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 401,
                     "message": "unauthorized"
                     }), 401
 
+
 '''
 error handling for Bad Request
 '''
+
+
 @app.errorhandler(400)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 400,
                     "message": "bad request"
                     }), 400
